e23ee94dc32b5441f24e64ea54c0d7276793c2b1
czf <122385930@qq.com>
2020-12-27 18:20:24 +0800

    代码监控

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ module.exports = {
         // 键必须以斜线开始和结束
         '/@/': path.resolve(__dirname, './src')
     },
-    hostname: '0.0.0.0',
+    // hostname: '0.0.0.0',
     port: 8080,
     // 是否自动在浏览器打开
     open: true,
